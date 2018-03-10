d2b2a4d2b6ded43de2cd164c279266a3c0a6bf74
Mikhail Pontus <m.pontus@gmail.com>
2018-03-10 10:36:30 +0300

    Rename bolt-highlights to bolt-mode-highlights

--- bolt-mode.el
+++ bolt-mode.el
@@ -29,7 +29,7 @@
 
 (defconst bolt-mode-identifier-re "[a-zA-Z_$][a-zA-Z0-9]+")
 
-(defvar bolt-highlights
+(defvar bolt-mode-highlights
   (let ((keywords '("type" "path" "extends" "is" "this" "now" "true" "false"))
 	(types '("String" "Number" "Boolean" "Object" "Any" "Null" "Map")))
     `((,(regexp-opt keywords 'words) . font-lock-keyword-face)
@@ -81,7 +81,7 @@
 ;;;###autoload
 (define-derived-mode bolt-mode fundamental-mode "Bolt"
   "Major mode for editing Bolt files"
-  (setq font-lock-defaults '(bolt-highlights))
+  (setq font-lock-defaults '(bolt-mode-highlights))
   (setq indent-line-function #'bolt-mode-indent-line))
 
 ;;;###autoload
